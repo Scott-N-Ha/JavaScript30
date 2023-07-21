8b090ed33fa2877d66f8579cdb9457de38c8dfac
Scott Ha <123viet123@gmail.com>
2023-07-21 16:30:05 -0700

    Still Trying to get GitHub Pages to work

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // build: {
-  //   emptyOutDir: false,
-  //   outDir: './',
-  // },
+  build: {
+    emptyOutDir: false,
+    outDir: './',
+  },
   plugins: [react()],
 })
